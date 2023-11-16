d37ad38e5a1d5a0fe144424b8fa0637481758004
Azadeh Yousefi <azadeh.yousefim@gmail.com>
2023-11-17 02:15:58 +0330

    send final project to github

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using PaggingMVC.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PaggingMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        PaggingMVCContext db = new PaggingMVCContext();
+        public ActionResult Index(int pageid=1)
+        {
+            int skip = (pageid-1)*3;
+            int count = db.People.Count();
+            ViewBag.PageID = pageid;
+            ViewBag.PageCount = count / 3;
+            var list = db.People.OrderBy(p=>p.ID).Skip(skip).Take(3).ToList();
+            return View(list);
+        }
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PaggingMVC.Models
+{
+    public class Person
+    {
+        [Key]
+        public int ID { get; set; }
+        [Display(Name = "Name")]
+        [Required(ErrorMessage = "please Enter {0}")]
+        [MaxLength(150)]
+        public string Name { get; set; }
+    }
+}
